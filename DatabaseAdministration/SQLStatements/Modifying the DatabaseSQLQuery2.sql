236592987de44efa0e7d7136294495b6e7edfb93
Lucky Manamela <lucky.manamela@sambeconsulting.com>
2025-02-18 14:49:31 +0200

    Modified the files

--- DatabaseAdministration/SQLStatements/Modifying the DatabaseSQLQuery2.sql
+++ DatabaseAdministration/SQLStatements/Modifying the DatabaseSQLQuery2.sql
@@ -2,11 +2,12 @@ ALTER DATABASE Sambe_Graduates2025
 ADD FILEGROUP secondary
 
 ALTER DATABASE Sambe_Graduates2025
-ADD FILE
+ADD FILE -- adding file 
 (
 NAME = Sambe_Graduates2025NewDataFile,
 FILENAME = 'C:\Mydatbase\Sambe_Graduates2025NewDataFile.ndf',
-SIZE = 10,
-MAXSIZE = 100,
-FILEGROWTH = 5)
+SIZE = 10, -- setting the initial size to 10mb
+MAXSIZE = 100, --Limits the maximum size of the file to 100 MB
+FILEGROWTH = 5 /*Specifies that the file will grow in increments of 5 MB whenever it runs out of space.
+Instead of setting a fixed size, SQL Server automatically expands the file as needed.*/)
 
--- DatabaseAdministration/SQLStatements/Remove Statement & Drop statement SQLQuery3.sql
+++ DatabaseAdministration/SQLStatements/Remove Statement & Drop statement SQLQuery3.sql
@@ -1,11 +1,11 @@
 ALTER DATABASE Sambe_Graduates2025
-REMOVE FILE Sambe_Graduates2025NewDataFile
+REMOVE FILE Sambe_Graduates2025NewDataFile--Removing the file  
 
 ALTER DATABASE Sambe_Graduates2025
 REMOVE FILEGROUP secondary
 
 USE master
 GO
-EXEC sp_detach_db "Sambe_Graduates2025"
+EXEC sp_detach_db "Sambe_Graduates2025" -- detaching the database
 GO
 DROP DATABASE Sambe_Graduates2025
