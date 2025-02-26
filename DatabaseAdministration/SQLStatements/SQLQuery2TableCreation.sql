1fea3f5542b1249c554cfdfe72445e56ce519cbc
Lucky Manamela <lucky.manamela@sambeconsulting.com>
2025-02-26 15:48:04 +0200

    Added a new table

---
+++ DatabaseAdministration/SQLStatements/SQLQuery2TableCreation.sql
@@ -0,0 +1,8 @@
+USE master
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    PhoneNumber VARCHAR(15)
+);
