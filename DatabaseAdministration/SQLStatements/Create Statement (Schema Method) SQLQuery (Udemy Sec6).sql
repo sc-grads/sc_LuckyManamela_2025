8d945e5fdc478cdc218e3dd9b1b04b8c5d102ed3
Lucky Manamela <lucky.manamela@sambeconsulting.com>
2025-03-11 10:45:01 +0200

    Added the Create statement using the Schema Method

---
+++ DatabaseAdministration/SQLStatements/Create Statement (Schema Method) SQLQuery (Udemy Sec6).sql
@@ -0,0 +1,23 @@
+--Creating a table in the AdventureWorks DB using The Schema method
+
+CREATE TABLE [AdventureWorks2016].[Sales].[StoreNew](
+Store_ID INT NOT NULL,
+Sales INT)
+GO
+
+
+--Altering the table StoreNew so that i can make Store_ID a primary key 
+ALTER TABLE [AdventureWorks2016].[Sales].[StoreNew]
+ADD CONSTRAINT PK_StoreNew PRIMARY KEY (Store_ID);
+GO
+
+--Creating a table named Visits on the Schema Sales and linking it to the StoreNew table
+CREATE TABLE [AdventureWorks2016].[Sales].[Visits](
+Visit_ID INT PRIMARY KEY IDENTITY(1,1),
+First_Name VARCHAR(50) NOT NULL,
+Last_Name VARCHAR(50) NOT NULL,
+Visit_Date DATETIME,
+Phone VARCHAR(20),
+Store_ID INT NOT NULL,
+FOREIGN KEY (Store_ID) REFERENCES Sales.StoreNew (Store_ID)
+)
