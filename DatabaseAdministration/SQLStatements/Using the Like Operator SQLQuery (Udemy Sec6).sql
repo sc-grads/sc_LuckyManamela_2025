bd3f0aa985463600dce161c79983103499a7c065
Lucky Manamela <lucky.manamela@sambeconsulting.com>
2025-03-11 14:51:04 +0200

    Added some note to the query to explain certain facts

--- DatabaseAdministration/SQLStatements/Using the Like Operator SQLQuery (Udemy Sec6).sql
+++ DatabaseAdministration/SQLStatements/Using the Like Operator SQLQuery (Udemy Sec6).sql
@@ -2,4 +2,11 @@
 SELECT *
 FROM Person.Person
 WHERE FirstName LIKE 'MAT%'
+GO
+
+--Accessing information where the FirstName ends with ew
+SELECT *
+FROM Person.Person
+WHERE FirstName LIKE '%ew' -- Take note that EW and ew is the same thing, MS SQl is not case sensetive
+
 
